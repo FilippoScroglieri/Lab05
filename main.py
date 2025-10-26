4ce26dfed1a6c8da090bbef73d174121aca97bcc
FilippoScroglieri <filippo.scroglieri05@gmail.com>
2025-10-26 14:56:05 +0100

    Lab05

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import flet as ft
+from flet.core.types import MainAxisAlignment, TextAlign
+
 from alert import AlertManager
 from autonoleggio import Autonoleggio
 
@@ -37,6 +39,21 @@ def main(page: ft.Page):
 
     # Tutti i TextField per le info necessarie per aggiungere una nuova automobile (marca, modello, anno, contatore posti)
     # TODO
+    text_Aggiungi= ft.Text('Aggiungi Nuova Automobile',size=20,text_align= ft.TextAlign.CENTER)
+
+    text_Marca= ft.TextField(value= 'Marca',width=200,text_size=15)
+
+    text_Modello = ft.TextField(value='Modello', width=200, text_size=15)
+
+    text_Anno = ft.TextField(value='Anno', width=200, text_size=15)
+
+    contatore= ft.TextField(width= 60, text_size=15)
+    contatore.value= 0
+
+
+
+
+
 
     # --- FUNZIONI APP ---
     def aggiorna_lista_auto():
@@ -59,6 +76,42 @@ def main(page: ft.Page):
 
     # Handlers per la gestione dei bottoni utili all'inserimento di una nuova auto
     # TODO
+    def HandlerMinus(e):
+        current_Val= contatore.value
+        if current_Val > 0:
+            current_Val= current_Val - 1
+            contatore.value= current_Val
+            page.update()
+
+    def HandlerPlus(e):
+        current_Val= contatore.value
+        current_Val= current_Val + 1
+        contatore.value= current_Val
+        page.update()
+
+    def Handler_aggiungi_auto(e):
+
+            if text_Anno.value.isdigit():
+                marca= text_Marca.value
+                modello= text_Modello.value
+                anno= int(text_Anno.value)
+                posti= contatore.value
+                autonoleggio.aggiungi_automobile(marca, modello, anno, posti)
+
+                text_Marca.value = ""
+                text_Modello.value = ""
+                text_Anno.value = ""
+                contatore.value = 0
+
+                aggiorna_lista_auto()
+                page.update()
+
+            else:
+                alert.show_alert('INSERISCI VALORI NUMERICI PER ANNO E POSTI')
+
+
+
+
 
     # --- EVENTI ---
     toggle_cambia_tema = ft.Switch(label="Tema scuro", value=True, on_change=cambia_tema)
@@ -66,6 +119,10 @@ def main(page: ft.Page):
 
     # Bottoni per la gestione dell'inserimento di una nuova auto
     # TODO
+    btnMinus= ft.IconButton(icon= ft.Icons.REMOVE,icon_size= 20, icon_color='red',on_click= HandlerMinus)
+    btnPlus = ft.IconButton(icon=ft.Icons.ADD, icon_size=20, icon_color='green', on_click=HandlerPlus)
+    bottone= ft.ElevatedButton('Aggiungi Automobile',animate_size= 24,on_click= Handler_aggiungi_auto)
+
 
     # --- LAYOUT ---
     page.add(
@@ -84,7 +141,8 @@ def main(page: ft.Page):
 
         # Sezione 3
         # TODO
-
+        text_Aggiungi, ft.Row([text_Marca, text_Modello, text_Anno,btnMinus,contatore,btnPlus],alignment=MainAxisAlignment.CENTER),
+        bottone,
         # Sezione 4
         ft.Divider(),
         ft.Text("Automobili", size=20),
